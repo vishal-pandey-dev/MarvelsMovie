b75a218556171b614b8598aa7428526075e9386b
Vishalpandey1247 <vp85623@gmail.com>
2020-10-13 16:17:21 +0530

    initial commit

--- Marvels/admin.py
+++ Marvels/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Character)
+admin.site.register(Phase)
+admin.site.register(Movie)
+admin.site.register(Production_company)
+admin.site.register(Producer)
+admin.site.register(Director)
+admin.site.register(Actor)
+admin.site.register(Connector)
--- Marvels/views.py
+++ Marvels/views.py
@@ -1,5 +1,80 @@
 from django.shortcuts import render
-
+from .models import *
+from django.template import loader
+from django.http import HttpResponse
 # Create your views here.
 def homepage(request):
       return render(request , 'marvels.html')
+      
+def allmovies(request):
+     movies=Movie.objects.all()   
+     comp=Production_company.objects.all() 
+     direct=Director.objects.all()
+     produce=Producer.objects.all()
+     act=Actor.objects.all() 
+     connector=Connector.objects.all()
+    
+     context = {
+                    'comp':comp,
+	                'movies':movies,
+	                'direct':direct,
+	                'produce':produce,
+	                'connector':connector,
+	                'act':act
+	             }
+     con=loader.get_template('allmovies.html')
+     
+     return HttpResponse(
+	              con.render(context , request)
+	)
+	
+	
+
+def ironman(request):
+	  return render (request , 'Ironman.html')
+
+def captainamerica(request):
+	  return render (request , 'captainamerica.html')
+	
+def thor(request):
+	  return render (request , 'Thor.html')
+	
+def hulk(request):
+	  return render (request , 'hulk.html')
+	
+def avengers(request):
+	  return render (request , 'Avengers.html')
+
+def spiderman(request):
+	  return render (request , 'spiderman.html')
+	
+def blackpanther(request):
+	  return render (request , 'blackpanther.html')
+	
+def doctorstrange(request):
+	  return render (request , 'doctorstrange.html')
+	
+def antman(request):
+	  return render (request , 'antman.html')
+	
+def guardians(request):
+	  return render (request , 'guardians.html')
+	
+def captainmarvel(request):
+	  return render (request , 'captainamerica.html')
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
